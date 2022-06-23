5f6fdc0b7b695e33dfa2cd33a4675cb55dd531d6
Paul Tiry <tirypaul@gmail.com>
2022-06-23 16:01:24 +0200

    made a variable character

--- js/player.mjs
+++ js/player.mjs
@@ -2,28 +2,35 @@ export default class Player {
   constructor() {
     this.positionX = 15 * 38
     this.positionY = 0
-    this.playerElement = this.createPlayer()
+    this.playerContainer = this.createPlayer()
+    this.image = this.createImage(this.playerContainer)
   }
   createPlayer() {
     const domPlayer = document.createElement('div')
-    const image = document.createElement('img')
-    image.id = 'character'
-    image.setAttribute('src', '../assets/images/character-right.png')
-    domPlayer.appendChild(image)
     domPlayer.id = 'player'
     domPlayer.style.left = `${this.positionX}px`
     const boardElm = document.getElementById('board')
     boardElm.appendChild(domPlayer)
     return domPlayer
   }
+  createImage(parent) {
+    const image = document.createElement('img')
+    image.id = 'character'
+    image.setAttribute('src', '../assets/images/character-right.png')
+    parent.appendChild(image)
+    return image
+  }
   moveLeft() {
-    if (this.positionX === 0) return
+    if (this.positionX <= 0) return
+    this.image.src = '../assets/images/character-left.png'
     this.positionX = this.positionX - 15
-    this.playerElement.style.left = `${this.positionX}px`
+    this.playerContainer.style.left = `${this.positionX}px`
   }
   moveRight() {
-    if (this.positionX === 1155) return
+    if (this.positionX >= 1125) return
+    this.image.src = '../assets/images/character-right.png'
     this.positionX = this.positionX + 15
-    this.playerElement.style.left = `${this.positionX}px`
+    this.playerContainer.style.left = `${this.positionX}px`
+    console.log(this.positionX)
   }
 }
